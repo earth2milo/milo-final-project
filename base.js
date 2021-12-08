5d27972a46e1650b70d0f900090a134109be0124
earth2milo <mas10085@nyu.edu>
2021-12-08 13:12:29 -0500

    base of my terrain generator!!
    highlights include making a 2 dimensional array in p5js!

--- base.js
+++ base.js
@@ -1,15 +1,11 @@
-let currentCamera;
-let camFrame1, camFrame2;
-
-let delta = 0.01;
 
 
 
 
 function preload() {
-  fRoboto = loadFont('https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf');	
-	spriteSheetTextureG=loadImage('data/sprite_sheet_grinch.png');
-	
+  fRoboto = loadFont('https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf');  
+  //spriteSheetTextureG=loadImage('data/sprite_sheet_grinch.png');
+  
 }
 
 function grinchPreload() {
@@ -20,63 +16,52 @@ function grinchPreload() {
 
 }
 
+let cols, rows; // colums and rows for building my grid
+let scale=20; // my constant
+let w=1200; // I fill more than the screen size because of how the 3d rendering functions with 2d drawings
+let h=1200;
 
-
+let terrain = []; // my array that will nest more arrays within it
 
 function setup() {
-createCanvas(100, 100, WEBGL); // set 800x800 window size to converse performance on all devices when rendering WEB GL
-  texture(spriteSheetTextureG);
-	
-camFrame1=createCamera();
-camFrame2=createCamera();
-camFrame2.setPosition(30,0,50);
-camFrame2.lookAt(0,0,0)
-camFrame2.ortho();
+createCanvas(800,800,WEBGL); // set 800x800 window size to converse performance on all devices when rendering WEB GL
+background(120,20,90);
 
-  currentCamera=1;
+//terrain[cols].push= w/scale;
+//terrain[rows].push= h/scale;
+cols= w/scale;
+rows= h/scale;
+for(let y=0; y < rows; y++) {  
+  terrain[y]=[];
+for(let x=0; x < cols; x++) {
+  terrain[y][x]= random(-10,10);
+}
+}
 }
 
 //initial cam position
+// this 3d Terrain Simulator is based off Dan Shiffman's Coding Challenge #11 in Processing
+// All code needed to be translated in p5js, and I wanted this to act as a solid foundation for texturing the terrain to fit Dr Seuss images.
+// link:https://www.youtube.com/watch?v=IKB1hWWedMk
 
 function draw() {
-	background(120,20,90);
-  camFrame1.lookAt(0,0,0);
- // camFrame1.setPosition(sin(frameCount/60)*200,0,100);
+  
+  background(120,20,90); 
+  stroke(255);
+  noFill();
+  rotateX(PI/3); // my rotation of 60 degrees 
+  translate(-w/2,-h/2); // moving my canvas to fit the grid fully
 
-  // every 100 frames switch between 2 cs
-	//camera(0,0,0,0,0,0,0,1,0); // my first three variables: Where is the camera's position? my next three: where is the camera looking? My last two values show which way is up
-	//camera(width/2, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
+  for(let y=0; y < rows; y++) { // double for loops for rows and columns
+      beginShape(TRIANGLE_STRIP); // theses 2 for loops create a perfect grid
+  for(let x=0; x < cols; x++) {
+      vertex(x*scale,y*scale, terrain[y][x]); // first variable is x and y, third is Z which will generate the "terrain" or mountains thanks to a random value
+      vertex(x*scale,(y+1)*scale,terrain[y][x]);
 
-  if(frameCount % 100 === 0); {
-  if(currentCamera === 1) {
-    setCamera(camFrame1);
-    currentCamera=0;
-  } else {
-setCamera(camFrame2);
-currentCamera=1;
-}
+
+
+    }
+endShape();
   }
-  introShapes();
-
-}
-
-
-
-function introShapes() {
-rotateX(frameCount * 0.01);
-  translate(-100, 0, 0);
-  box(20);
-  translate(35, 0, 0);
-  box(20);
-  translate(35, 0, 0);
-  box(20);
-  translate(35, 0, 0);
-  box(20);
-  translate(35, 0, 0);
-  box(20);
-  translate(35, 0, 0);
-  box(20);
-  translate(35, 0, 0);
-  box(20);
-
+  
 }
