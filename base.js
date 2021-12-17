019df487d3a99e5c51036e5700cec1bc06337096
earth2milo <mas10085@nyu.edu>
2021-12-17 16:30:40 -0500

    flying balloons!

--- base.js
+++ base.js
@@ -7,15 +7,21 @@ const startTimer = 120
 let secs = startTimer;
 let frames = 60;
 let alpha=255;
-
+let sceneTracker;
+let intro, intro_create;
 
 
 function preload() {
   fRoboto = loadFont('https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf');
   //spriteSheetTextureG=loadImage('data/sprite_sheet_grinch.png');
-  grass = loadImage('data/Grass.png');
+  girl= loadImage('data/girl.png');
   hump = loadImage('data/hump.png');
   trees = loadModel('data/tree_3d.obj');
+  elephant= loadImage('data/elephant.png');
+  balloon= loadImage('data/balloon.png');
+  things= loadImage('data/dr_seuss_things.png');
+  cat = loadImage('data/dr_seuss_cat.png');
+ // intro= loadImage('data/intro.gif');
 
 }
 
@@ -33,10 +39,7 @@ let terrain = []; // my array that will nest  2 more arrays within it
 function setup() {
   createCanvas(800, 800, WEBGL); // set 800x800 window size to converse performance on all devices when rendering WEB GL
   frameRate(frames);
-  //background(120,20,90);
-  //textureWrap(CLAMP,0);
-  //pg=createGraphics(600,600);
-  //grass=createImage(400,400);
+
   cols = w / scale;
   rows = h / scale;
 
@@ -49,12 +52,21 @@ function setup() {
 
 function draw() {
 background(11, 13, 250);
-box(20);
-console.log(frameCount);
-if(frameCount=6000) {
+sceneTracker=0;
+//if (millis === 10000) {
+//console.log(sceneTracker);
+//sceneTracker=1;
+//push();
 
+//texture(intro);
+//plane(intro.width-300, intro.height-300);
+//pop();
+//intro_create.position(50,350);
+//}
+
+//if(sceneTracker === 1) {
 landscape();
-}
+//}
 
 }
 
@@ -68,7 +80,7 @@ function landscape() {
   flying-=0.1;
  // }
 
-  background(11,13,250); 
+ // background(11,13,250); 
   stroke(0,alpha);
   fill(23,250,23);
   rotateX(PI/2.3); // my rotation of 60 degrees 
@@ -80,7 +92,9 @@ let xoff = 0;
   terrain[y]=[];
 for(let x=0; x < cols; x++) {
   //terrain[y][x]= random(-10,10);
-  terrain[y][x]= map(noise(xoff,yoff),0,1,-5,50); // my noise function essentially will help my terrain look more "organic" as one random value will be surrounded by related value and so forth.
+
+  terrain[y][x]= map(noise(xoff,yoff),0,1,-5,70); // my noise function essentially will help my terrain look more "organic" as one random value will be surrounded by related value and so forth.
+  alpha=map(terrain[y][x],-5,50,90,255);
 xoff += 0.1; // to make my perlin noise values more structued, i make a xoff and yoff values to help structure the terrain consistently
 }
 yoff += 0.1;
@@ -91,7 +105,7 @@ yoff += 0.1;
       beginShape(TRIANGLE_STRIP); // theses 2 for loops create a perfect grid
     
   for(let x=0; x < cols; x++) {
-    if(terrain[y][x]>=38) {
+    if(terrain[y][x]>=48) {
 push();
 ambientLight(255);
 noStroke();
@@ -100,9 +114,22 @@ translate(x*scale,y*scale,-terrain[y][x]);
 //translate()
 //rotateX(30);
 rotateY(250);
-rotateZ(300);
+rotateZ(400);
 
-if(terrain[y][x]>=40) {
+if(terrain[y][x]>=52) {
+push();
+translate(x*scale+150,y*scale+20, terrain[y][x]+2);
+translate(100,100);
+rotateX(18);
+rotateY(-20);
+rotateZ(-350);
+texture(balloon);
+plane(balloon.width-800, balloon.height-800);
+pop();
+
+}
+
+if(terrain[y][x]>=52) {
 ambientMaterial(255,245,111); // lorax tree colors!
   } else {
 ambientMaterial(242,111,119);
