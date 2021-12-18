47a19f066cc689957608150e4f1387cc8f92a0ec
earth2milo <mas10085@nyu.edu>
2021-12-18 01:14:30 -0500

    intro sequences loaded into videos
    tried to make my two intro text sequences into gifs but had to switch to video. Now have to code on open processing and texteditor as my browser will not allow autoplay!

--- base.js
+++ base.js
@@ -7,13 +7,14 @@ const startTimer = 120
 let secs = startTimer;
 let frames = 60;
 let alpha=255;
-let sceneTracker;
-let intro, intro_create;
+let sceneTracker=1;
+let gif_loadIntro, gif_createIntro;
+let gif_loadContinued, gif_createContinued;
+let vid;
 
 
 function preload() {
   fRoboto = loadFont('https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf');
-  //spriteSheetTextureG=loadImage('data/sprite_sheet_grinch.png');
   girl= loadImage('data/girl.png');
   hump = loadImage('data/hump.png');
   trees = loadModel('data/tree_3d.obj');
@@ -21,10 +22,17 @@ function preload() {
   balloon= loadImage('data/balloon.png');
   things= loadImage('data/dr_seuss_things.png');
   cat = loadImage('data/dr_seuss_cat.png');
- // intro= loadImage('data/intro.gif');
-
+  //gif_loadIntro= loadImage('data/intro.mp4');
+  //gif_createIntro= createImg('data/intro.gif');
+  // gif_loadContinued= loadImage('data/intro_continued.mp4');
+   //gif_createContinued= createImg('data/intro_continued.gif');
+ vid=createVideo(['data/intro.mp4','data/intro_continued.mp4'],vidLoad);
 }
 
+function vidLoad() { // callback for my videos
+vid.loop();
+vid.volume();
+}
 
 
 let cols, rows; // colums and rows for building my grid
@@ -39,7 +47,7 @@ let terrain = []; // my array that will nest  2 more arrays within it
 function setup() {
   createCanvas(800, 800, WEBGL); // set 800x800 window size to converse performance on all devices when rendering WEB GL
   frameRate(frames);
-
+sceneTracker=0;
   cols = w / scale;
   rows = h / scale;
 
@@ -52,21 +60,14 @@ function setup() {
 
 function draw() {
 background(11, 13, 250);
-sceneTracker=0;
-//if (millis === 10000) {
-//console.log(sceneTracker);
-//sceneTracker=1;
-//push();
 
-//texture(intro);
-//plane(intro.width-300, intro.height-300);
-//pop();
-//intro_create.position(50,350);
-//}
+vid.size(windowWidth,windowHeight);
+vid.play(); // had trouble with vid.play since browsers like chrome would automatically block it from playing
 
-//if(sceneTracker === 1) {
+
+if(sceneTracker === 3) {
 landscape();
-//}
+}
 
 }
 
@@ -75,14 +76,12 @@ landscape();
 
 
 function landscape() {
+flying-=0.1;
 
-//if(frameCount>=300) {
-  flying-=0.1;
- // }
 
- // background(11,13,250); 
-  stroke(0,alpha);
-  fill(23,250,23);
+ 
+  stroke(0,0);
+  fill(23,alpha,23);
   rotateX(PI/2.3); // my rotation of 60 degrees 
   translate(-w/2,-h/2); // moving my canvas to fit the grid fully
 
@@ -94,7 +93,7 @@ for(let x=0; x < cols; x++) {
   //terrain[y][x]= random(-10,10);
 
   terrain[y][x]= map(noise(xoff,yoff),0,1,-5,70); // my noise function essentially will help my terrain look more "organic" as one random value will be surrounded by related value and so forth.
-  alpha=map(terrain[y][x],-5,50,90,255);
+  alpha=map(terrain[y][x],-5,50,180,250);
 xoff += 0.1; // to make my perlin noise values more structued, i make a xoff and yoff values to help structure the terrain consistently
 }
 yoff += 0.1;
@@ -105,20 +104,20 @@ yoff += 0.1;
       beginShape(TRIANGLE_STRIP); // theses 2 for loops create a perfect grid
     
   for(let x=0; x < cols; x++) {
+
     if(terrain[y][x]>=48) {
+
 push();
 ambientLight(255);
 noStroke();
 translate(-300,-300,0);
 translate(x*scale,y*scale,-terrain[y][x]);
-//translate()
-//rotateX(30);
 rotateY(250);
-rotateZ(400);
+rotateZ(300);
 
-if(terrain[y][x]>=52) {
+if(terrain[y][x]>=52) { // my balloons in the distance!
 push();
-translate(x*scale+150,y*scale+20, terrain[y][x]+2);
+translate(x*scale+15,y*scale+20, terrain[y][x]+2);
 translate(100,100);
 rotateX(18);
 rotateY(-20);
@@ -128,42 +127,54 @@ plane(balloon.width-800, balloon.height-800);
 pop();
 
 }
+if(terrain[y][x]>=60) {
+push();
+texture(elephant);
+rotateY(-180);
+rotateX(10);
+translate(-280,100,300);
+plane(elephant.width-800,elephant.height-800);
+pop();
+}
 
-if(terrain[y][x]>=52) {
+
+if(terrain[y][x]>=52) { // my tree colors!
 ambientMaterial(255,245,111); // lorax tree colors!
   } else {
 ambientMaterial(242,111,119);
 }
 model(trees);
 pop();
+
     }
+
+
       vertex(x*scale,y*scale, terrain[y][x]); // first variable is x and y, third is Z which will generate the "terrain" or mountains thanks to a random value
       vertex(x*scale,(y+1)*scale,terrain[y+1][x]);
 
 
 
     }
+
+
 endShape();
   }
+
+
+
+
+
+
+
 }
+
   
 
-
-
-
-
-class movingImages  {
-
-constructor() {
-
+function mousePressed() {
+  sceneTracker+1;
 
 
 }
 
 
 
-
-
-
-
-}
